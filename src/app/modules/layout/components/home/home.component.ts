0506150f50d432c3c9658111888147d1f293225c
Gaurav Khatri <gaurav.khatri@ajm.in>
2025-03-19 18:18:26 +0530

    some changes

--- src/app/modules/layout/components/home/home.component.ts
+++ src/app/modules/layout/components/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('videoElement') video!: ElementRef<HTMLVideoElement>;
 
+  ngAfterViewInit() {
+    if (this.video) {
+      this.video.nativeElement.play().catch(error => console.log("Autoplay blocked:", error));
+    } else {
+      console.log("Video element not found!");
+    }
+  }
 }
